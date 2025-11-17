3ae4d2949fa939fec53e256989b350a13735fb95
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-11-16 23:56:48 -0500

    Create functions.js

---
+++ src/lib/functions.js
@@ -0,0 +1,17 @@
+import settings from '../settings.json' assert { type: 'json' };
+
+export function applyFont(text, fontType) {
+    const fontMap = new Map();
+    const normalLetters = settings.fonts.letters.split(' ');
+    const targetLetters = settings.fonts[fontType].split(' ');
+
+    normalLetters.forEach((letter, index) => {
+        fontMap.set(letter, targetLetters[index]);
+    });
+
+    return text.toLowerCase().split('').map(char => fontMap.get(char) || char).join('');
+}
+
+export function getRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
